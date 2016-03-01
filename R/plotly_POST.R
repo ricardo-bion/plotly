c8307d21c17defdee2c55a154027ad243aa51efd
yankev <home@homes-air.zyxel.com>
2016-03-01 10:31:02 -0500

    changed all hidden to secret

--- R/plotly_POST.R
+++ R/plotly_POST.R
@@ -4,25 +4,25 @@
 #' \url{https://plot.ly/rest/}
 #'
 #' @param x either a ggplot object, a plotly object, or a list.
-#' @param filename character string describing the name of the plot in your 
-#' plotly account. Use / to specify directories. If a directory path does not 
-#' exist it will be created. If this argument is not specified and the title 
+#' @param filename character string describing the name of the plot in your
+#' plotly account. Use / to specify directories. If a directory path does not
+#' exist it will be created. If this argument is not specified and the title
 #' of the plot exists, that will be used for the filename.
-#' @param fileopt character string describing whether to create a "new" plotly, 
-#' "overwrite" an existing plotly, "append" data to existing plotly, 
+#' @param fileopt character string describing whether to create a "new" plotly,
+#' "overwrite" an existing plotly, "append" data to existing plotly,
 #' or "extend" it.
-#' @param sharing If 'public', anyone can view this graph. It will appear in 
+#' @param sharing If 'public', anyone can view this graph. It will appear in
 #' your profile and can appear in search engines. You do not need to be
 #' logged in to Plotly to view this chart.
 #' If 'private', only you can view this plot. It will not appear in the
-#' Plotly feed, your profile, or search engines. You must be logged in to 
-#' Plotly to view this graph. You can privately share this graph with other 
-#' Plotly users in your online Plotly account and they will need to be logged 
+#' Plotly feed, your profile, or search engines. You must be logged in to
+#' Plotly to view this graph. You can privately share this graph with other
+#' Plotly users in your online Plotly account and they will need to be logged
 #' in to view this plot.
-#' If 'hidden', anyone with this hidden link can view this chart. It will
-#' not appear in the Plotly feed, your profile, or search engines. 
-#' If it is embedded inside a webpage or an IPython notebook, anybody who is 
-#' viewing that page will be able to view the graph. 
+#' If 'secret', anyone with this secret link can view this chart. It will
+#' not appear in the Plotly feed, your profile, or search engines.
+#' If it is embedded inside a webpage or an IPython notebook, anybody who is
+#' viewing that page will be able to view the graph.
 #' You do not need to be logged in to view this plot.
 #' @export
 #' @seealso \link{plot_ly}, \link{signup}
@@ -35,16 +35,16 @@
 #' plotly_POST(p, filename = "mtcars-bar-plot")
 #' }
 
-plotly_POST <- function(x, filename, fileopt = "new", 
-                        sharing = c("public", "private", "hidden")) {
+plotly_POST <- function(x, filename, fileopt = "new",
+                        sharing = c("public", "private", "secret")) {
   x <- plotly_build(x)
-  x$filename <- if (!missing(filename)) { 
+  x$filename <- if (!missing(filename)) {
     filename
   } else {
     # try our damndest to assign a sensible filename
-    x$filename %||% as.character(x$layout$title) %||% 
-      paste(c(x$layout$xaxis$title, x$layout$yaxis$title, x$layout$zaxis$title), 
-            collapse = " vs. ") %||% "plot from api" 
+    x$filename %||% as.character(x$layout$title) %||%
+      paste(c(x$layout$xaxis$title, x$layout$yaxis$title, x$layout$zaxis$title),
+            collapse = " vs. ") %||% "plot from api"
   }
   if (!is.null(x$fileopt)) {
     warning("fileopt was specified in the wrong place.",
@@ -54,11 +54,11 @@ plotly_POST <- function(x, filename, fileopt = "new",
   if (!is.null(x$world_readable)) {
     warning('world_readable is no longer supported. Instead, set the sharing\n',
             'argument to "private" (you must be logged in to access),\n',
-            '"hidden" (anybody with the obscured URL can access) or "public"\n', 
+            '"secret" (anybody with the obscured URL can access) or "public"\n',
             '(anybody can view).')
   }
   x$world_readable <- if (sharing[1] == "public") TRUE else FALSE
-  
+
   # plotly server has trouble with empty properties
   x <- x[sapply(x, length) > 0]
   # construct body of message to plotly server
@@ -74,10 +74,10 @@ plotly_POST <- function(x, filename, fileopt = "new",
   base_url <- file.path(get_domain(), "clientresp")
   resp <- httr::POST(base_url, body = bod)
   con <- process(struct(resp, "clientresp"))
-  if (sharing[1] == "hidden") {
+  if (sharing[1] == "secret") {
     bits <- strsplit(con$url, "/")[[1]]
     plot_id <- bits[length(bits)]
-    url <- file.path(get_domain("api"), "v2", "files", 
+    url <- file.path(get_domain("api"), "v2", "files",
                      paste0(verify("username"), ":", plot_id))
     bod <- list(share_key_enabled = TRUE)
     con2 <- httr::PATCH(url, plotly_headers("v2"), body = bod, encode = "json")
